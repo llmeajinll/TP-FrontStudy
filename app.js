ef4d59391da105cf2202ddf89ea914125e966596
김혜진 <khj000217@naver.com>
2022-11-25 21:58:00 +0900

    part 4-2 마침

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 
-function onLoginSubmit() {
-  const username = loginInput.value;
-  console.log(username);
-  // input option에 다 넣어줌
-  //   if (username === '') {
-  //     alert('Please write your name');
-  //   } else if (username.length > 15) {
-  //     alert('Your name is too long.');
-  //   }
+function onLoginSubmit(tomato) {
+  tomato.preventDefault();
+  console.log(loginInput.value);
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);
