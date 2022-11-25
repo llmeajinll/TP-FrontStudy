2323e6b14bc16bb3207435b3564dcdcd02537651
김혜진 <khj000217@naver.com>
2022-11-25 22:11:01 +0900

    part 4-3 마침

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 
-function onLoginSubmit(tomato) {
-  tomato.preventDefault();
+const link = document.querySelector('a');
+
+function onLoginSubmit(event) {
+  event.preventDefault();
   console.log(loginInput.value);
 }
 
+// function handleLinkClick(event) {
+//   event.preventDefault();
+//   console.log(event);
+//   console.log('clicked!');
+// }
+
 loginForm.addEventListener('submit', onLoginSubmit);
+// link.addEventListener('click', handleLinkClick);
